a3bb19420f02ada9fbc9aae61028a66e125a22e2
Ivanzitus <bielivan219@gmail.com>
2025-07-03 19:23:30 +0000

    adição de fotos ao jonso

--- app/models/eventos_model.py
+++ app/models/eventos_model.py
@@ -4,20 +4,24 @@ def ordenados_por_data():
     conexao = conectar()
     cursor = conexao.cursor(dictionary=True)
     sql = """
-            SELECT
-            id,
-            titulo,
-            descricao,
-            local,
-            data,
-            dt_fim,
-            aberto,
-            site,
-            dt_cadastro
-            FROM
-            Eventos
-            ORDER BY
-            data ASC;
+SELECT
+    E.id,
+    E.titulo,
+    E.descricao,
+    E.local,
+    E.data,
+    E.dt_fim,
+    E.aberto,
+    E.site,
+    E.dt_cadastro,
+    I.caminho AS caminho_imagem_capa, -- Retorna o caminho da imagem, ou NULL se não houver
+    I.legenda AS legenda_imagem_capa   -- Opcional: Retorna a legenda da imagem também
+FROM
+    Eventos AS E
+LEFT JOIN
+    Imagens AS I ON I.origem_id = E.id AND I.capa = 1
+ORDER BY
+    E.data ASC;
             """
     cursor.execute(sql)
     eventos = cursor.fetchall()
