742f60af5f9df480d309c3ae5edc9c4fe03aefb3
Murilo Ramos <151067196+MuriloRamos007@users.noreply.github.com>
2025-07-16 20:03:02 -0400

    Update locais_controller.py
    Signed-off-by: Murilo Ramos <151067196+MuriloRamos007@users.noreply.github.com>

--- app/controllers/locais_controller.py
+++ app/controllers/locais_controller.py
@@ -1,23 +1,32 @@
-import hashlib
-import app.models.usuario_model as model
+import app.models.locais_model as model
 
-def listar_todos():
-    return model.listar()
- 
-def buscar(id):
-    return model.buscar(id)
+def listar_top4(grupo, tipo):
+    tipo = tipo or ""
+    return model.listar_top4(grupo, tipo)
 
-def criar(dados):
-    senhaTxt = dados['senha']
-    dados['senha'] = hashlib.sha256(senhaTxt.encode()).hexdigest() 
-    return model.criar(dados)
+def buscar_por_id(id):
+    return model.buscar_por_id(id)
 
-def atualizar(dados):
-    
-    if dados['senha']:
-        hash = hashlib.sha256(dados['senha'].encode()).hexdigest() 
-        model.atualizar_senha(dados['id'], hash)
-    return model.atualizar(dados)
+def listar_pontos_turisticos(categoria, subcategoria):
+    return model.listar_pontos_turisticos(categoria, subcategoria)
 
-def deletar(id):
-    return model.deletar(id)
+def listar_estabelecimentos(categoria, subcategoria):
+    return model.listar_estabelecimentos(categoria, subcategoria)
+
+def inserir_ponto_turistico(dados):
+    return model.inserir_ponto_turistico(dados)
+
+def atualizar_ponto_turistico(id, dados):
+    return model.atualizar_ponto_turistico(id, dados)
+
+def deletar_ponto_turistico(id):
+    return model.deletar_ponto_turistico(id)
+
+def inserir_estabelecimento(dados):
+    return model.inserir_estabelecimento(dados)
+
+def atualizar_estabelecimento(id, dados):
+    return model.atualizar_estabelecimento(id, dados)
+
+def deletar_estabelecimento(id):
+    return model.deletar_estabelecimento(id)
