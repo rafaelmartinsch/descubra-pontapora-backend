5559103334d30de79d8dc3e22e033c96a395324a
MuriloRamos007 <murilowiirr@gmail.com>
2025-07-28 21:48:16 -0400

    documentação em historia_model

--- app/models/historia_model.py
+++ app/models/historia_model.py
@@ -30,6 +30,15 @@ def buscar_por_id(id):
     return resultado
 
 def criar(data):
+    """
+    Cadastro de uma nova história ao BD.
+    Args:
+        data (dict): Conjunto de dados para serem alterados.
+    Returns:
+        tuple: dados do título, conteúdo, autor e data de atualização serão criados.
+            - 
+            
+    """
     conexao = conectar()
     cursor = conexao.cursor()
     sql = "INSERT INTO historias (titulo, texto, autor, dt_cadastro) VALUES (%s, %s, %s, %s)"
@@ -47,6 +56,18 @@ def criar(data):
     return id_novo
 
 def atualizar(id, data):
+    """
+    Atualiza uma história já publicada, onde apenas admin e editores têm permissão.
+    Args:
+        id (int): ID da história em específico.
+        data (dict): Conjunto de dados para serem alterados.
+    Returns:
+        tuple: dados do título, conteúdo, autor e data de atualização serão mudados.
+            - str: Título da história.
+            - str: Conteúdo da história.
+            - str: Autor da mudança.
+            - datetime: Data da atualização.
+    """
     conexao = conectar()
     cursor = conexao.cursor()
     sql = """
@@ -68,6 +89,13 @@ def atualizar(id, data):
     return {"mensagem": "Notícia atualizada com sucesso"}
 
 def deletar(id):
+    """
+    Exclui uma história unitariamente por base de um id
+    Args:
+        id (int): Id da história em específico
+    Returns:
+        None: 'Notícia removida com sucesso'
+    """
     conexao = conectar()
     cursor = conexao.cursor()
     cursor.execute("DELETE FROM historias WHERE id = %s", (id,))
@@ -77,6 +105,14 @@ def deletar(id):
     return {"mensagem": "Notícia removida com sucesso"}
 
 def listar_autores():
+    """
+    Retorna uma lista de administradores e editores para registrar um editar uma história.
+        - 'A': Admin
+        - 'E': Editor
+    Returns:
+        list: lista de admins e editores. 
+    """
+    pass
     conexao = conectar()
     cursor = conexao.cursor(dictionary=True)
     cursor.execute("SELECT id, nome FROM usuarios WHERE tipo IN ('A', 'E') ORDER BY nome")
